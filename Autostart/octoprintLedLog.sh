6a4a16675ab7d84589ab77c1df0ffdfb878894d0
Christian V <44880451+StoKatze@users.noreply.github.com>
2022-10-19 17:54:31 +0200

    Update octoprintLedLog.sh

--- Autostart/octoprintLedLog.sh
+++ Autostart/octoprintLedLog.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-python3 AutomaticOctoprintLED.py > ./log.txt 2>&1
+# Set correct work path (the one this script is in)
+cd "${0%/*}"
+# Execute the python script with the unbuffered python option for real time logging to file
+python3 -u ./AutomaticOctoprintLED.py > ./log.txt 2>&1 
